c9d6d5e61d1ef7b8f5b27d6657da3c6c9aa8b14e
Faiz Authar <faizauthar@gmail.com>
2021-01-29 17:34:33 +0700

    MovieZ: ShowEntitiy: Initial model

---
+++ app/src/main/java/io/faizauthar12/moviez/data/ShowEntity.kt
@@ -0,0 +1,9 @@
+package io.faizauthar12.moviez.data
+
+data class ShowEntity(
+        var showsId: String,
+        var title: String,
+        var description: String,
+        var releaseYear: String,
+        var imagePath: String
+)
