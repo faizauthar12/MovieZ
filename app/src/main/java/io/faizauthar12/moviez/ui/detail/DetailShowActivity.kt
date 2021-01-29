bc3f6b21c9dbc2e1dd4e428efe747028865ea0b3
Faiz Authar <faizauthar@gmail.com>
2021-01-29 15:50:58 +0700

    MovieZ: Detail: Add binding support

--- app/src/main/java/io/faizauthar12/moviez/ui/detail/DetailShowActivity.kt
+++ app/src/main/java/io/faizauthar12/moviez/ui/detail/DetailShowActivity.kt
@@ -2,7 +2,8 @@ package io.faizauthar12.moviez.ui.detail
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import io.faizauthar12.moviez.R
+import io.faizauthar12.moviez.databinding.ActivityDetailShowBinding
+import io.faizauthar12.moviez.databinding.ContentDetailShowBinding
 
 class DetailShowActivity : AppCompatActivity() {
 
@@ -10,11 +11,16 @@ class DetailShowActivity : AppCompatActivity() {
         const val EXTRA_SHOW = "extra_show"
     }
 
+    private lateinit var detailContentBinding: ContentDetailShowBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_show)
-        setSupportActionBar(findViewById(R.id.toolbar))
+        val activityDetailShowBinding = ActivityDetailShowBinding.inflate(layoutInflater)
+        detailContentBinding = activityDetailShowBinding.detailContent
 
+        setContentView(activityDetailShowBinding.root)
+
+        setSupportActionBar(activityDetailShowBinding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
