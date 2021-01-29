530df6547a1d47b58709e830db26c4e79d3fe76d
Faiz Authar <faizauthar@gmail.com>
2021-01-29 15:54:19 +0700

    MovieZ: Home: Add binding support

--- app/src/main/java/io/faizauthar12/moviez/ui/home/HomeActivity.kt
+++ app/src/main/java/io/faizauthar12/moviez/ui/home/HomeActivity.kt
@@ -3,10 +3,13 @@ package io.faizauthar12.moviez.ui.home
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import io.faizauthar12.moviez.R
+import io.faizauthar12.moviez.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+        val activityHomeBinding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(activityHomeBinding.root)
     }
 }
