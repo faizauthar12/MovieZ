4a27ebbe7e45f486cbad8a16c4cadaf45c78194a
Faiz Authar <faizauthar@gmail.com>
2021-01-31 08:29:58 +0700

    MovieZ: Movies RecycleView[5/5]: Implement into the MoviesFragment

--- app/src/main/java/io/faizauthar12/moviez/ui/movies/MoviesFragment.kt
+++ app/src/main/java/io/faizauthar12/moviez/ui/movies/MoviesFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import io.faizauthar12.moviez.R
 import io.faizauthar12.moviez.databinding.FragmentMoviesBinding
 
@@ -17,5 +19,23 @@ class MoviesFragment : Fragment() {
         // Binding support
         fragmentMoviesBinding = FragmentMoviesBinding.inflate(layoutInflater, container,false)
         return fragmentMoviesBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (activity != null) {
+            val viewModel = ViewModelProvider(this,ViewModelProvider.NewInstanceFactory())[MoviesViewModel::class.java]
+            val movies = viewModel.getMovies()
+
+            val moviesAdapter = MoviesAdapter()
+            moviesAdapter.setMovies(movies)
+
+            with(fragmentMoviesBinding.rvMovie) {
+                layoutManager = LinearLayoutManager(context)
+                setHasFixedSize(true)
+                adapter = moviesAdapter
+            }
+        }
     }
 }
