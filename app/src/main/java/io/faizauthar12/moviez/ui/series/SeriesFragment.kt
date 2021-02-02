d11262b565fce92e91f8a5b1ac2145779a8d693d
Faiz Authar <faizauthar@gmail.com>
2021-02-02 11:44:06 +0700

    MovieZ: Series RecycleView[5/5]: Implement into the SeriesFragment

--- app/src/main/java/io/faizauthar12/moviez/ui/series/SeriesFragment.kt
+++ app/src/main/java/io/faizauthar12/moviez/ui/series/SeriesFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import io.faizauthar12.moviez.databinding.FragmentSeriesBinding
 
 class SeriesFragment : Fragment() {
@@ -16,5 +18,23 @@ class SeriesFragment : Fragment() {
         // Binding support
         fragmentSeriesBinding = FragmentSeriesBinding.inflate(layoutInflater, container, false)
         return fragmentSeriesBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (activity != null){
+            val viewModel = ViewModelProvider(this,ViewModelProvider.NewInstanceFactory())[SeriesViewModel::class.java]
+            val series = viewModel.getSeries()
+
+            val seriesAdapter = SeriesAdapter()
+            seriesAdapter.setSeries(series)
+
+            with(fragmentSeriesBinding.rvSeries){
+                layoutManager = LinearLayoutManager(context)
+                setHasFixedSize(true)
+                adapter = seriesAdapter
+            }
+        }
     }
 }
