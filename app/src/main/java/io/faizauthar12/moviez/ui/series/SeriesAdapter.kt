cf1b1f48bb87e0e255360cc7387ea15c475e11f9
Faiz Authar <faizauthar@gmail.com>
2021-02-02 11:43:38 +0700

    MovieZ: Series RecycleView[3/5]: Initial SeriesAdapter

---
+++ app/src/main/java/io/faizauthar12/moviez/ui/series/SeriesAdapter.kt
@@ -0,0 +1,48 @@
+package io.faizauthar12.moviez.ui.series
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import io.faizauthar12.moviez.R
+import io.faizauthar12.moviez.data.ShowEntity
+import io.faizauthar12.moviez.databinding.ItemsSeriesBinding
+
+class SeriesAdapter : RecyclerView.Adapter<SeriesAdapter.SeriesViewHolder>(){
+
+    private val listSeries = ArrayList<ShowEntity>()
+
+    fun setSeries(series: List<ShowEntity>?){
+        if (series == null) return
+        this.listSeries.clear()
+        this.listSeries.addAll(series)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SeriesViewHolder {
+        val itemsSeriesBinding = ItemsSeriesBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return SeriesViewHolder(itemsSeriesBinding)
+    }
+
+    override fun onBindViewHolder(holder: SeriesViewHolder, position: Int) {
+        val series = listSeries[position]
+        holder.bind(series)
+    }
+
+    override fun getItemCount(): Int = listSeries.size
+
+    class SeriesViewHolder(private val binding: ItemsSeriesBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(series: ShowEntity){
+            with(binding){
+                tvSeriesTitle.text = series.title
+                tvSeriesRelease.text = series.releaseYear
+                tvOverview.text = series.description
+                Glide.with(itemView.context)
+                        .load(series.imagePath)
+                        .apply(RequestOptions.placeholderOf(R.drawable.ic_loading)
+                                .error(R.drawable.ic_error))
+                        .into(imgPoster)
+            }
+        }
+    }
+}
