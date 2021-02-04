07629f45280cecb8262d0e52984fbfde3349c742
Faiz Authar <faizauthar@gmail.com>
2021-02-04 17:00:47 +0700

    MovieZ: Splash Screen: Update fullscreen logic

--- app/src/main/java/io/faizauthar12/moviez/ui/splash/SplashActivity.kt
+++ app/src/main/java/io/faizauthar12/moviez/ui/splash/SplashActivity.kt
@@ -1,10 +1,12 @@
 package io.faizauthar12.moviez.ui.splash
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.WindowInsets
 import android.view.WindowManager
 import io.faizauthar12.moviez.R
 import io.faizauthar12.moviez.ui.home.HomeActivity
@@ -14,11 +16,19 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        // Hide Statusbar and set it full screen
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
+        /*
+            To get fullscreen we should check the SDK first.
+            SDK 21 >= 29 use deprecated flag
+         */
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
 
         // Delayed the splash screen
         Handler(Looper.getMainLooper()).postDelayed({
