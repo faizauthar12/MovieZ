637ef22d592f38f773083c5645d9d3254b46e73a
Faiz Authar <faizauthar@gmail.com>
2021-02-10 21:01:43 +0700

    MovieZ: Repository[1/9]: Create ModelResponse

---
+++ app/src/main/java/io/faizauthar12/moviez/data/source/remote/response/SerieResponse.kt
@@ -0,0 +1,13 @@
+package io.faizauthar12.moviez.data.source.remote.response
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class SerieResponse (
+        var showsId: String,
+        var title: String,
+        var description: String,
+        var releaseYear: String,
+        var imagePath: String
+) : Parcelable
