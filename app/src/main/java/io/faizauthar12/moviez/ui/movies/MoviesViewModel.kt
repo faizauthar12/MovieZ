576053aaa3b281c599d9ddded403ae902472b5fd
Faiz Authar <faizauthar@gmail.com>
2021-02-16 20:05:40 +0700

    MovieZ: LiveData[4/6]: Implement LiveData into {Movies/Series}ViewModel

--- app/src/main/java/io/faizauthar12/moviez/ui/movies/MoviesViewModel.kt
+++ app/src/main/java/io/faizauthar12/moviez/ui/movies/MoviesViewModel.kt
@@ -1,10 +1,11 @@
 package io.faizauthar12.moviez.ui.movies
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import io.faizauthar12.moviez.data.MovieZRepository
 import io.faizauthar12.moviez.data.source.local.entity.ShowEntity
 
 class MoviesViewModel(private val movieZRepository: MovieZRepository) : ViewModel() {
 
-    fun getMovies(): List<ShowEntity> = movieZRepository.getAllMovies()
+    fun getMovies(): LiveData<List<ShowEntity>> = movieZRepository.getAllMovies()
 }
--- app/src/main/java/io/faizauthar12/moviez/ui/series/SeriesViewModel.kt
+++ app/src/main/java/io/faizauthar12/moviez/ui/series/SeriesViewModel.kt
@@ -1,9 +1,10 @@
 package io.faizauthar12.moviez.ui.series
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import io.faizauthar12.moviez.data.MovieZRepository
 import io.faizauthar12.moviez.data.source.local.entity.ShowEntity
 
 class SeriesViewModel(private val movieZRepository: MovieZRepository) : ViewModel() {
-    fun getSeries(): List<ShowEntity> = movieZRepository.getAllSeries()
+    fun getSeries(): LiveData<List<ShowEntity>> = movieZRepository.getAllSeries()
 }
