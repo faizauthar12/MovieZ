d533144f978988b113a00e687a217322a6bdd091
Faiz Authar <faizauthar@gmail.com>
2021-02-16 20:04:49 +0700

    MovieZ: LiveData[3/6]: Implement LiveData into MovieZRepository

--- app/src/main/java/io/faizauthar12/moviez/data/MovieZRepository.kt
+++ app/src/main/java/io/faizauthar12/moviez/data/MovieZRepository.kt
@@ -1,7 +1,11 @@
 package io.faizauthar12.moviez.data
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import io.faizauthar12.moviez.data.source.local.entity.ShowEntity
 import io.faizauthar12.moviez.data.source.remote.RemoteDataSource
+import io.faizauthar12.moviez.data.source.remote.response.MovieResponse
+import io.faizauthar12.moviez.data.source.remote.response.SerieResponse
 
 class MovieZRepository private constructor(private val remoteDataSource: RemoteDataSource): MovieZDataSource {
 
@@ -14,35 +18,45 @@ class MovieZRepository private constructor(private val remoteDataSource: RemoteD
                 }
     }
 
-    override fun getAllMovies(): ArrayList<ShowEntity> {
-        val movieResponse = remoteDataSource.getAllMovies()
-        val movieList = ArrayList<ShowEntity>()
-        for (response in movieResponse) {
-            val movie = ShowEntity(
-                    response.showsId,
-                    response.title,
-                    response.description,
-                    response.releaseYear,
-                    response.imagePath
-            )
-            movieList.add(movie)
-        }
-        return movieList
+    override fun getAllMovies(): LiveData<List<ShowEntity>> {
+        val movieResults = MutableLiveData<List<ShowEntity>>()
+        remoteDataSource.getAllMovies(object : RemoteDataSource.LoadMoviesCallback {
+            override fun onAllMoviesReceived(movieResponses: List<MovieResponse>) {
+                val movieList = ArrayList<ShowEntity>()
+                for (response in movieResponses) {
+                    val movie = ShowEntity(
+                        response.showsId,
+                        response.title,
+                        response.description,
+                        response.releaseYear,
+                        response.imagePath
+                    )
+                    movieList.add(movie)
+                }
+                movieResults.postValue(movieList)
+            }
+        })
+        return movieResults
     }
 
-    override fun getAllSeries(): ArrayList<ShowEntity> {
-        val serieResponse = remoteDataSource.getAllSeries()
-        val serieList = ArrayList<ShowEntity>()
-        for (response in serieResponse) {
-            val serie = ShowEntity(
-                    response.showsId,
-                    response.title,
-                    response.description,
-                    response.releaseYear,
-                    response.imagePath
-            )
-            serieList.add(serie)
-        }
-        return serieList
+    override fun getAllSeries(): LiveData<List<ShowEntity>> {
+        val serieResults = MutableLiveData<List<ShowEntity>>()
+        remoteDataSource.getAllSeries(object : RemoteDataSource.LoadSeriesCallback {
+            override fun onAllSeriesReceived(serieResponses: List<SerieResponse>) {
+                val serieList = ArrayList<ShowEntity>()
+                for (response in serieResponses) {
+                    val serie = ShowEntity(
+                        response.showsId,
+                        response.title,
+                        response.description,
+                        response.releaseYear,
+                        response.imagePath
+                    )
+                    serieList.add(serie)
+                }
+                serieResults.postValue(serieList)
+            }
+        })
+        return serieResults
     }
 }
