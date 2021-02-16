ad9944da4a18dcb262f24df3a9c9047f1d27b45e
Faiz Authar <faizauthar@gmail.com>
2021-02-16 17:42:47 +0700

    MovieZ: local entiity: make local model Parcelable

--- app/src/main/java/io/faizauthar12/moviez/data/source/local/entity/ShowEntity.kt
+++ app/src/main/java/io/faizauthar12/moviez/data/source/local/entity/ShowEntity.kt
@@ -1,9 +1,13 @@
 package io.faizauthar12.moviez.data.source.local.entity
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class ShowEntity(
         var showsId: String,
         var title: String,
         var description: String,
         var releaseYear: String,
         var imagePath: String
-)
+) : Parcelable
