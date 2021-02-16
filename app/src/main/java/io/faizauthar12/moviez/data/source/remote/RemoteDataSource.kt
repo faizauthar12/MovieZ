231a4022d5c2917747b8c0c50e4b96b68e0b9519
Faiz Authar <faizauthar@gmail.com>
2021-02-16 20:03:38 +0700

    MovieZ: LiveData[1/6]: Implement LiveData at local/remote/RemoteDataSource

--- app/src/main/java/io/faizauthar12/moviez/data/source/remote/RemoteDataSource.kt
+++ app/src/main/java/io/faizauthar12/moviez/data/source/remote/RemoteDataSource.kt
@@ -1,12 +1,18 @@
 package io.faizauthar12.moviez.data.source.remote
 
+import android.os.Handler
+import android.os.Looper
 import io.faizauthar12.moviez.data.source.remote.response.MovieResponse
 import io.faizauthar12.moviez.data.source.remote.response.SerieResponse
 import io.faizauthar12.moviez.utils.JsonHelper
 
 class RemoteDataSource private constructor(private val jsonHelper: JsonHelper){
 
+    private val handler = Handler(Looper.getMainLooper())
+
     companion object {
+        private const val SERVICE_LATENCY_IN_MILLIS: Long = 2000
+
         @Volatile
         private var instance: RemoteDataSource? = null
 
@@ -16,7 +22,19 @@ class RemoteDataSource private constructor(private val jsonHelper: JsonHelper){
                 }
     }
 
-    fun getAllMovies(): List<MovieResponse> = jsonHelper.loadMovies()
+    fun getAllMovies(callback: LoadMoviesCallback) {
+        handler.postDelayed({ callback.onAllMoviesReceived(jsonHelper.loadMovies())}, SERVICE_LATENCY_IN_MILLIS)
+    }
 
-    fun getAllSeries(): List<SerieResponse> = jsonHelper.loadSeries()
+    fun getAllSeries(callback: LoadSeriesCallback) {
+        handler.postDelayed({ callback.onAllSeriesReceived(jsonHelper.loadSeries())}, SERVICE_LATENCY_IN_MILLIS)
+    }
+
+    interface LoadMoviesCallback {
+        fun onAllMoviesReceived(movieResponses: List<MovieResponse>)
+    }
+
+    interface LoadSeriesCallback {
+        fun onAllSeriesReceived(serieResponses: List<SerieResponse>)
+    }
 }
