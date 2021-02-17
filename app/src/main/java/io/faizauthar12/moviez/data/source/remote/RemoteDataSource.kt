961a16776772b17f7344d3a968e54ccb93217df1
Faiz Authar <faizauthar@gmail.com>
2021-02-17 15:13:37 +0700

    MovieZ: API[3/6]: Refactor RemoteDataSource

--- app/src/main/java/io/faizauthar12/moviez/data/source/remote/RemoteDataSource.kt
+++ app/src/main/java/io/faizauthar12/moviez/data/source/remote/RemoteDataSource.kt
@@ -1,44 +1,46 @@
 package io.faizauthar12.moviez.data.source.remote
 
-import android.os.Handler
-import android.os.Looper
-import io.faizauthar12.moviez.utils.EspressoIdlingResource
+import io.faizauthar12.moviez.data.source.remote.request.ApiRequest
+import io.faizauthar12.moviez.data.source.remote.request.ApiRequest.Companion.API_KEY
+import io.faizauthar12.moviez.data.source.remote.request.ApiRequest.Companion.BASE_URL
+import io.faizauthar12.moviez.data.source.remote.request.ApiRequest.Companion.LANGUAGE_PREF
+import io.faizauthar12.moviez.data.source.remote.request.ApiRequest.Companion.PAGE
+import io.faizauthar12.moviez.data.source.remote.response.DataResponse
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class RemoteDataSource {
-
-    private val handler = Handler(Looper.getMainLooper())
 
     companion object {
         @Volatile
         private var instance: RemoteDataSource? = null
 
         fun getInstance(): RemoteDataSource =
-                instance ?: synchronized(this) {
-                    instance ?: RemoteDataSource()
-                }
+            instance ?: synchronized(this) {
+                instance ?: RemoteDataSource()
+            }
+
+        fun getApiService(): ApiRequest {
+            val loggingInterceptor =
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+            val client = OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .build()
+            val retrofit = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
+            return retrofit.create(ApiRequest::class.java)
+        }
     }
 
-    fun getAllMovies(callback: LoadMoviesCallback) {
-        EspressoIdlingResource.increment()
-        handler.postDelayed({
-            callback.onAllMoviesReceived(jsonHelper.loadMovies())
-            EspressoIdlingResource.decrement()
-        }, SERVICE_LATENCY_IN_MILLIS)
-    }
+    fun getAllMovies(): Call<DataResponse> =
+        getApiService().getAllMovies(API_KEY, LANGUAGE_PREF, PAGE)
 
-    fun getAllSeries(callback: LoadSeriesCallback) {
-        EspressoIdlingResource.increment()
-        handler.postDelayed({
-            callback.onAllSeriesReceived(jsonHelper.loadSeries())
-            EspressoIdlingResource.decrement()
-        }, SERVICE_LATENCY_IN_MILLIS)
-    }
-
-    interface LoadMoviesCallback {
-        fun onAllMoviesReceived(movieResponses: List<MovieResponse>)
-    }
-
-    interface LoadSeriesCallback {
-        fun onAllSeriesReceived(serieResponses: List<SerieResponse>)
-    }
+    fun getAllSeries(): Call<DataResponse> =
+        getApiService().getAllSeries(API_KEY, LANGUAGE_PREF, PAGE)
 }
