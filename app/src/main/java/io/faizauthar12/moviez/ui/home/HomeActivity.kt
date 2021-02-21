55b259c70404348cdb502eab774b9432db6fa88e
Faiz Authar <faizauthar@gmail.com>
2021-02-21 11:28:34 +0700

    MovieZ: BottomNavigation[2/2]: Implement bottom naviigation into logic

--- app/src/main/java/io/faizauthar12/moviez/ui/home/HomeActivity.kt
+++ app/src/main/java/io/faizauthar12/moviez/ui/home/HomeActivity.kt
@@ -2,7 +2,14 @@ package io.faizauthar12.moviez.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.NonNull
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import io.faizauthar12.moviez.R
 import io.faizauthar12.moviez.databinding.ActivityHomeBinding
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,10 +18,22 @@ class HomeActivity : AppCompatActivity() {
         val activityHomeBinding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(activityHomeBinding.root)
 
-        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
-        activityHomeBinding.viewPager.adapter = sectionsPagerAdapter
-        activityHomeBinding.tabs.setupWithViewPager(activityHomeBinding.viewPager)
+        setupBottomNavigation(activityHomeBinding)
 
         supportActionBar?.elevation = 0f
+    }
+
+    private fun setupBottomNavigation(activityHomeBinding: ActivityHomeBinding) {
+        val navView: BottomNavigationView = activityHomeBinding.navView
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration.Builder(
+            R.id.seriesFragment, R.id.moviesFragment
+        ).build()
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
